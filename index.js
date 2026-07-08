652f18b27b58aeb5c87519f3a47d0bcdc5d55e0c
Elishell79 <eshell@daltonstate.edu>
2026-07-08 13:04:17 -0400

    Initialize React application with index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Integral application doman
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
